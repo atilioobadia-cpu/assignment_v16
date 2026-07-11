5c0d48eb11cce7f7f0dd81b9c99d3d1e73ee0ebc
atilioobadia-cpu <atilioobadia@gmail.com>
2026-07-11 16:58:49 +0300

    Bugfix: SLA validate date comparison type mismatch (use getdate)

--- alpha_assignment_mgmt/alpha_assignment_management/doctype/alpha_engagement_sla/alpha_engagement_sla.py
+++ alpha_assignment_mgmt/alpha_assignment_management/doctype/alpha_engagement_sla/alpha_engagement_sla.py
@@ -1,10 +1,10 @@
 import frappe
 from frappe.model.document import Document
-from frappe.utils import today
+from frappe.utils import today, getdate
 
 class AlphaEngagementSLA(Document):
     def validate(self):
-        if self.alpha_processing_deadline and self.alpha_processing_deadline < today():
+        if self.alpha_processing_deadline and getdate(self.alpha_processing_deadline) < getdate(today()):
             if self.status != "Closed":
                 self.status = "Breached"
                 self.breach_notified = 1
