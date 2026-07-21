986c6a080b61479bb3e0f2d6447151997c46dcc9
atilioobadia-cpu <atilioobadia@gmail.com>
2026-07-21 11:08:42 +0300

    fix: force delayed=False instead of setdefault
    setdefault doesn't override the existing delayed=True from
    sendmail_input_dict. Force kwargs['delayed'] = False instead
    so emails truly go out immediately.

--- alpha_assignment_mgmt/__init__.py
+++ alpha_assignment_mgmt/__init__.py
@@ -6,7 +6,7 @@ frappe.sendmail_default_delayed = True
 _sendmail = frappe.sendmail
 
 def _sendmail_now(*args, **kwargs):
-	kwargs.setdefault("delayed", False)
+	kwargs["delayed"] = False
 	return _sendmail(*args, **kwargs)
 
 frappe.sendmail = _sendmail_now
